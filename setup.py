5fae589ed0c1f794d95b5fc5081cf0b3d649ec45
Aakash <akashmittal0207@gmail.com>
2023-02-18 00:47:12 +0530

    setup file update

--- setup.py
+++ setup.py
@@ -1,21 +1,29 @@
-#useful for distribution of source code as Liberary format.
 
 from setuptools import find_packages,setup
 
+from typing import List
+
 REQUIREMENT_FILE_NAME="requirements.txt"
+HYPHEN_E_DOT = "-e ."
 
 def get_requirements()->List[str]:
     
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
         requirement_list = requirement_file.readlines()
     requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
-    #repla
+    
+    if HYPHEN_E_DOT in requirement_list:
+        requirement_list.remove(HYPHEN_E_DOT)
+    return requirement_list
+
+
 
 setup(
     name="sensor",
     version="0.0.1",
     author="Aakash",
     author_email="aakashmittal0207@gmail.com",
-    packages = find_packages(), #this find_packages search for Python source code wherever it finds that code it will convert into packages.  
-    install_requires=get_requirements(),
-)
+    packages = find_packages(),  #this find_packages search for Python source code wherever it finds that code it will convert into packages.
+    install_requires = get_requirements(),
+) 
+
